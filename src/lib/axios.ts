d595b74f4ca7eada14baa2e34c30e5ba7e93cb9d
Joshua Kim <itartist.jk@gmail.com>
2021-05-19 13:10:36 +0900

    feat: create pure cms axios instant

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import AxiosMockAdapter from 'axios-mock-adapter';
 
 export const CMSURL = 'http://103.244.108.203:1337';
+export const CMS_TOKEN = 'sbcnepqm22@0';
 export const APIURL = 'http://103.244.108.41:3000/api';
 
 const axiosInstance = axios.create({
@@ -19,6 +20,10 @@ export const mock = new AxiosMockAdapter(axiosInstance, { delayResponse: 0 });
 
 export const apiServer = axios.create({
   baseURL: APIURL,
+});
+
+export const cmsServer = axios.create({
+  baseURL: CMSURL,
 });
 
 apiServer.interceptors.response.use(
