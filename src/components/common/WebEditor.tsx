d497e0e7d0f502f02d4123f8a1d0d63dc954bddd
SangKwon <sangkwon@innofin.co.kr>
2021-12-30 14:39:13 +0900

    fix:웹 에디터 image copy&paste 기능을 수정하다

--- src/components/common/WebEditor.tsx
+++ src/components/common/WebEditor.tsx
@@ -25,18 +25,82 @@ const WebEditor: React.FC<IWebEditorProps> = (props) => {
         //@ts-ignore
         selector: 'textarea#full-featured',
         language: 'ko_KR',
-        height: 700,
+        plugins:
+          'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount imagetools textpattern noneditable help formatpainter permanentpen pageembed charmap quickbars emoticons advtable export',
+        tinydrive_token_provider: 'URL_TO_YOUR_TOKEN_PROVIDER',
+        tinydrive_dropbox_app_key: 'YOUR_DROPBOX_APP_KEY',
+        tinydrive_google_drive_key: 'YOUR_GOOGLE_DRIVE_KEY',
+        tinydrive_google_drive_client_id:
+          'YOUR_GOOGLE_DRIVE_CLIENT_ID',
+        mobile: {
+          plugins:
+            'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount textpattern noneditable help formatpainter pageembed charmap quickbars emoticons advtable',
+        },
+        menu: {
+          tc: {
+            title: 'Comments',
+            items: 'addcomment showcomments deleteallconversations',
+          },
+        },
+        menubar: 'file edit view insert format tools table tc help',
+        toolbar:
+          'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
         autosave_ask_before_unload: true,
         autosave_interval: '30s',
         autosave_prefix: '{path}{query}-{id}-',
         autosave_restore_when_empty: false,
         autosave_retention: '2m',
-        menubar: false,
+        image_advtab: true,
+        link_list: [
+          { title: 'My page 1', value: 'https://www.tiny.cloud' },
+          { title: 'My page 2', value: 'http://www.moxiecode.com' },
+        ],
+        image_list: [
+          { title: 'My page 1', value: 'https://www.tiny.cloud' },
+          { title: 'My page 2', value: 'http://www.moxiecode.com' },
+        ],
+        image_class_list: [
+          { title: 'None', value: '' },
+          { title: 'Some class', value: 'class-name' },
+        ],
+        importcss_append: true,
+        templates: [
+          {
+            title: 'New Table',
+            description: 'creates a new table',
+            content:
+              '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>',
+          },
+          {
+            title: 'Starting my story',
+            description: 'A cure for writers block',
+            content: 'Once upon a time...',
+          },
+          {
+            title: 'New list with dates',
+            description: 'New List with dates',
+            content:
+              '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>',
+          },
+        ],
+        template_cdate_format:
+          '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+        template_mdate_format:
+          '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+        height: 600,
         image_caption: true,
-        paste_as_text: true,
-        automatic_uploads: true,
-        paste_data_images: true,
-        file_picker_types: 'file image media',
+        quickbars_selection_toolbar:
+          'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+        noneditable_noneditable_class: 'mceNonEditable',
+        toolbar_mode: 'sliding',
+        spellchecker_ignore_list: ['Ephox', 'Moxiecode'],
+        tinycomments_mode: 'embedded',
+        content_style: '.mymention{ color: gray; }',
+        contextmenu:
+          'link image imagetools table configurepermanentpen',
+        a11y_advanced_options: true,
+        skin: useDarkMode ? 'oxide-dark' : 'oxide',
+        content_css: useDarkMode ? 'dark' : 'default',
         images_upload_handler: async function (
           blobInfo,
           success,
@@ -91,38 +155,6 @@ const WebEditor: React.FC<IWebEditorProps> = (props) => {
           };
           input.click();
         },
-        importcss_append: true,
-        mobile: {
-          plugins:
-            'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount textpattern noneditable help formatpainter pageembed charmap quickbars emoticons advtable',
-        },
-        menu: {
-          tc: {
-            title: 'Comments',
-            items: 'addcomment showcomments deleteallconversations',
-          },
-        },
-        toolbar_mode: 'sliding',
-        spellchecker_ignore_list: ['Ephox', 'Moxiecode'],
-        template_cdate_format:
-          '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
-        template_mdate_format:
-          '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
-        content_style: '.mymention{ color: gray; }',
-        browser_spellcheck: true,
-        block_unsupported_drop: true,
-        image_title: true,
-        mentions_selector: '.mymention',
-        mentions_item_type: 'profile',
-        contextmenu:
-          'link image imagetools table configurepermanentpen',
-        a11y_advanced_options: true,
-        skin: useDarkMode ? 'oxide-dark' : 'oxide',
-        content_css: useDarkMode ? 'dark' : 'default',
-        plugins:
-          'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists checklist wordcount textpattern noneditable help formatpainter permanentpen pageembed charmap quickbars emoticons advtable export',
-        toolbar:
-          'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
       }}
     />
   );
