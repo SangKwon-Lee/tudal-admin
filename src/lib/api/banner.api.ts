fefd8cacd575520ce9333c094aec6aab0c4035d7
SangKwon <sangkwon@innofin.co.kr>
2022-05-18 08:26:13 +0900

    fix:배너등록 수정

--- src/lib/api/banner.api.ts
+++ src/lib/api/banner.api.ts
@@ -1,13 +1,13 @@
 import axios from 'src/lib/axios';
 
 export const postOneImage = async (input) => {
-  return axios.post(`/settings/hidden-report/banners`, input);
+  return axios.post(`/settings/master-report/banners`, input);
 };
 
 export const getBanners = async () => {
-  return axios.get(`/settings/hidden-report/banners`);
+  return axios.get(`/settings/master-report/banners`);
 };
 
 export const putBanner = async (input) => {
-  return axios.put(`/settings/hidden-report/banners`, input);
+  return axios.put(`/settings/master-report/banners`, input);
 };
