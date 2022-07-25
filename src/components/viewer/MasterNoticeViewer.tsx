9dce8179f82c1bd17966e4c15f2e61098631dac8
SangKwon <sangkwon@innofin.co.kr>
2022-07-25 13:38:00 +0900

    fix:공지사항 글자색 수정

--- src/components/viewer/MasterNoticeViewer.tsx
+++ src/components/viewer/MasterNoticeViewer.tsx
@@ -2,7 +2,10 @@ import type { FC } from 'react';
 import { Box, Container, Typography } from '@material-ui/core';
 import { Viewer } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
-
+import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
+import 'tui-color-picker/dist/tui-color-picker.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+// import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 interface Props {
   masterNotice: any;
 }
@@ -11,13 +14,16 @@ const MasterNoticeViewerPresenter: FC<Props> = (props) => {
   const { masterNotice } = props;
 
   return (
-    <Box sx={{ backgroundColor: 'white' }}>
+    <Box>
       {/* <Container maxWidth="md"> */}
-      <Typography variant="h3">{masterNotice.title}</Typography>
+      <Typography variant="h4" sx={{ color: 'white' }}>
+        {masterNotice.title}
+      </Typography>
       {/* <Typography variant="h6" style={{ textAlign: 'right' }}>
           {masterNotice.title}
         </Typography> */}
       <Viewer
+        theme="dark"
         initialValue={masterNotice.contents}
         // plugins={[colorSyntax]}
       />
