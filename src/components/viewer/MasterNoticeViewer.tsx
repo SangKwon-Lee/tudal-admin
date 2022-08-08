f959e2d257f4a3ea6ac06e871e9468bbeae4a96f
SangKwon <sangkwon@innofin.co.kr>
2022-08-08 13:58:10 +0900

    fix:공지사항 배경색을 변경하다

--- src/components/viewer/MasterNoticeViewer.tsx
+++ src/components/viewer/MasterNoticeViewer.tsx
@@ -1,11 +1,10 @@
 import type { FC } from 'react';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Container, Typography } from '@material-ui/core';
 import { Viewer } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
 import 'tui-color-picker/dist/tui-color-picker.css';
 import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
-// import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 interface Props {
   masterNotice: any;
 }
@@ -14,20 +13,13 @@ const MasterNoticeViewerPresenter: FC<Props> = (props) => {
   const { masterNotice } = props;
 
   return (
-    <Box>
-      {/* <Container maxWidth="md"> */}
-      {/* <Typography variant="h4" sx={{ color: 'white' }}>
-        {masterNotice.title}
-      </Typography> */}
-      {/* <Typography variant="h6" style={{ textAlign: 'right' }}>
-          {masterNotice.title}
-        </Typography> */}
-      <Viewer
-        theme="dark"
-        initialValue={masterNotice.contents}
-        // plugins={[colorSyntax]}
-      />
-      {/* </Container> */}
+    <Box sx={{ py: 3, backgroundColor: 'white' }}>
+      <Container maxWidth="md">
+        <Viewer
+          initialValue={masterNotice.contents}
+          //plugins={[colorSyntax]}
+        />
+      </Container>
     </Box>
   );
 };
