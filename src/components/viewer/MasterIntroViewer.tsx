1f3474f19fcb3a0602bbc7d1b8db0c44085e88c0
SangKwon <sangkwon@innofin.co.kr>
2022-09-01 09:47:11 +0900

    fix:공지사항 글자색 수정

--- src/components/viewer/MasterIntroViewer.tsx
+++ src/components/viewer/MasterIntroViewer.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react';
-import { Box, Container } from '@material-ui/core';
+import { Box, Container, useTheme } from '@material-ui/core';
 import { Viewer } from '@toast-ui/react-editor';
 import { IMaster } from 'src/types/master';
 import '@toast-ui/editor/dist/toastui-editor.css';
@@ -10,13 +10,15 @@ interface Props {
 
 const MasterIntroViewer: FC<Props> = (props) => {
   const { master } = props;
-
+  const theme = useTheme();
   return (
-    <Box sx={{ py: 3, backgroundColor: 'white' }}>
+    <Box
+      sx={{ py: 3, backgroundColor: theme.palette.background.paper }}
+    >
       <Container maxWidth="md">
         <Viewer
           initialValue={master.intro}
-          //plugins={[colorSyntax]}
+          theme={theme.palette.mode}
         />
       </Container>
     </Box>
--- src/components/viewer/MasterNoticeViewer.tsx
+++ src/components/viewer/MasterNoticeViewer.tsx
@@ -1,23 +1,24 @@
 import type { FC } from 'react';
-import { Box, Container, Typography } from '@material-ui/core';
+import { Box, Container, useTheme } from '@material-ui/core';
 import { Viewer } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
 import 'tui-color-picker/dist/tui-color-picker.css';
 import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+
 interface Props {
   masterNotice: any;
 }
 
 const MasterNoticeViewerPresenter: FC<Props> = (props) => {
   const { masterNotice } = props;
-
+  const theme = useTheme();
   return (
-    <Box sx={{ py: 3, backgroundColor: 'white' }}>
+    <Box sx={{ py: 3 }}>
       <Container maxWidth="md">
         <Viewer
           initialValue={masterNotice.contents}
-          //plugins={[colorSyntax]}
+          theme={theme.palette.mode}
         />
       </Container>
     </Box>
