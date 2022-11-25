8e2dcd66a827f2b8b46dc8cd326682905239c17a
SangKwon <sangkwon@innofin.co.kr>
2022-11-25 10:46:34 +0900

    fix:env 설정을 다시 되돌리다

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
-const { REACT_APP_API_URL } = process.env;
+const { REACT_APP_CMS_TOKEN, REACT_APP_CMS_URL, REACT_APP_API_URL } =
+  process.env;
 
-export const CMSURL = 'https://cms.tudal.co.kr';
-export const CMS_TOKEN = 'xnwkdmlekfdlsuser@020';
+export const CMSURL = REACT_APP_CMS_URL;
+export const CMS_TOKEN = REACT_APP_CMS_TOKEN;
 export const APIURL = REACT_APP_API_URL;
 
 const axiosInstance = axios.create({
