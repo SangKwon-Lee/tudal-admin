bd1bd95c76648192fb26c350b5112a29737237ff
Sangkwon-Lee <rnrel13@gmail.com>
2023-03-16 08:59:02 +0900

    fix:한경 주소를 변경하다

--- src/components/dashboard/hankyung/list/HankyungList.Container.tsx
+++ src/components/dashboard/hankyung/list/HankyungList.Container.tsx
@@ -276,10 +276,10 @@ export default function HankyungListContainer() {
           stocks: result.map((data) => data.data.id),
           targetURL:
             category === '시초먹기'
-              ? 'https://wow-oneshot.innofin.co.kr/?scrollTo=시초'
+              ? 'wow-oneshot.innofin.co.kr/?scrollTo=시초'
               : category === '장중먹기'
-              ? 'https://wow-oneshot.innofin.co.kr/?scrollTo=장중'
-              : 'https://wow-oneshot.innofin.co.kr/?scrollTo=종가',
+              ? 'wow-oneshot.innofin.co.kr/?scrollTo=장중'
+              : 'wow-oneshot.innofin.co.kr/?scrollTo=종가',
         });
         if (status === 200) {
           toast.success('등록 됐습니다.');
@@ -332,10 +332,10 @@ export default function HankyungListContainer() {
         stocks: result.map((data) => data.data.id),
         targetURL:
           category === '시초먹기'
-            ? 'https://wow-oneshot.innofin.co.kr/?scrollTo=시초'
+            ? 'wow-oneshot.innofin.co.kr/?scrollTo=시초'
             : category === '장중먹기'
-            ? 'https://wow-oneshot.innofin.co.kr/?scrollTo=장중'
-            : 'https://wow-oneshot.innofin.co.kr/?scrollTo=종가',
+            ? 'wow-oneshot.innofin.co.kr/?scrollTo=장중'
+            : 'wow-oneshot.innofin.co.kr/?scrollTo=종가',
       });
       if (status === 200) {
         toast.success('수정 됐습니다.');
